333f98d1e61a6e2ad19c61d74f559d79689fbac1
simsulison <54574014+simsulison@users.noreply.github.com>
2021-10-07 17:34:06 +0900

    FEAT : #1 Implement Skeleton Code For Project

---
+++ src/main/java/com/senti/bert/BertApplication.java
@@ -0,0 +1,13 @@
+package com.senti.bert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BertApplication.class, args);
+	}
+
+}
